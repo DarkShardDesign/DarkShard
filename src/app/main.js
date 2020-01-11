5e6a91ae3cf575d0d776b27b2b9db73a5595d632
Sean Przybyla <sean@darksharddesign.co.uk>
2020-01-11 17:48:12 +0100

    registered test route

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,10 @@
+import Router from './core/router';
 import App from './app';
 
 // register route's
+Router.set('/home', route => console.log(route));
+
+Router.init();
 
 // launch the app
 App.launch();
