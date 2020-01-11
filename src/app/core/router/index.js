c7ea630aed6d1218059b9362450ab33a629f0e08
Sean Przybyla <sean@darksharddesign.co.uk>
2020-01-11 19:10:19 +0100

    updated router to use components not callbacks

--- src/app/core/router/index.js
+++ src/app/core/router/index.js
@@ -1,35 +1,48 @@
 import { isString, isFunction } from '../utils/typechecks';
 
-class Router {
+export default class Router {
     constructor () {
+        this.currentComponent = {};
         this.routes = [];
     }
 
     /**
-     * Register a new route uri and callback function
+     * Register a new route uri and component
      * @param {string} uri the uri to match against for the given route
-     * @param {Function} callback the callback to invoke when we match the route
+     * @param {Component} component the component to create and mount when we match the route
      */
-    set(uri, callback) {
-        // we need to have values for both uri and callback
-        if (!uri || !callback) throw new Error('uri and callback must be given');
+    set(uri, component) {
+        // we need to have values for both uri and component
+        if (!uri || !component) throw new Error('uri and component must be given');
 
         // they need to be the correct types
         if (!isString(uri)) throw new Error('uri must be a string');
-        if (!isFunction(callback)) throw new Error('callback must be a function');
+        if (!isFunction(component)) throw new Error('component must be a function');
 
-        // we dont want conflicting routes, so if we already have it throw and error
+        // we dont want conflicting routes, so if we already have it throw an error
         if (this.routes.find(route => route.uri === uri)) throw new Error(`the uri ${uri} already exists`);
 
         // add the new route
         this.routes.push({
             uri,
-            callback
+            component
         });
     }
 
     /**
-     * initialise the router
+     * Register and mount the given route/component
+     * @param {Object} route the route object that holds the component to use
+     * @param {string} path the path that triggered this component
+     */
+    mountComponent(route, path) {
+        if(this.currentComponent.unmount) this.currentComponent.unmount();
+        this.currentComponent = new route.component();
+        this.currentComponent.register();
+        this.currentComponent.mount(path);
+    }
+
+    /**
+     * initialise the router and mount the matching component
      */
     init() {
         // process routes until we find a match
@@ -40,9 +53,7 @@ class Router {
             let path = window.location.pathname;
 
             // if the path matches our route then invoke its callback
-            if (path.match(regEx)) return route.callback.call(this, { path });
+            if (path.match(regEx)) return this.mountComponent(route, path);
         });
     }
 }
-
-export default new Router();
